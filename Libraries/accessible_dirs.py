d33164940177484c7dd714b7b39628b8edf1f71e
Aidan_Shim <aidanshim0204@gmail.com>
2024-02-13 16:37:58 -0800

    Tiny directory changes

---
+++ Libraries/accessible_dirs.py
@@ -0,0 +1,5 @@
+import sys 
+import os
+
+dirs =['C:\Users\aidan\OneDrive\Documents', 'C:\\Users\aidan\Downloads', 'C:\Users\aidan\OneDrive\Desktop']
+new_dirs =['C:\Users\aidan\OneDrive\Pictures\Downloaded-Photos', 'C:\Users\aidan\OneDrive\Desktop\Academic Docs\General_Docs']
